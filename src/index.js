c4c6d4b9bff8f210533bad8bc7593c5c200a2d0f
shauandsss <shauands@gmail.com>
2021-04-17 00:45:07 -0300

    Doc Title & Icon

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+document.title = "Netflix";
 
 ReactDOM.render( 
   <React.StrictMode>
-  <App/>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
